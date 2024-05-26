d0f912dad28f34994ab62120d90f95e63c9d9aae
ziippy <ziippy@naver.com>
2024-05-26 15:36:57 +0900

    chat 샘플렝 ChatOpenAI 연동 적용

--- 03_retrieval/06_chat_1.py
+++ 03_retrieval/06_chat_1.py
@@ -1,5 +1,10 @@
 import chainlit as cl
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
 
+chat = ChatOpenAI(
+    model="gpt-4o",
+)
 
 @cl.on_chat_start #← 채팅이 시작될 때 실행할 함수를 정의
 async def on_chat_start():
@@ -8,7 +13,14 @@ async def on_chat_start():
 @cl.on_message #← 메시지를 보낼 때 실행할 함수를 정의
 async def on_message(input_message):
     print("입력된 메시지: " + input_message)
-    await cl.Message(content="안녕하세요!").send() #← 챗봇의 답변을 보냄
+
+    result = chat(
+        [
+            HumanMessage(content=input_message)
+        ]
+    )
+    await cl.Message(content=result.content).send()  # ← 챗봇의 답변을 보냄
+    # await cl.Message(content="안녕하세요!").send() #← 챗봇의 답변을 보냄
 
 
 
